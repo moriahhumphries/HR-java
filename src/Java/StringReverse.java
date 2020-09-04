074de7d84019dfda0389699705e78afdb6f9b22f
Moriah Humphries <moriah.humphries@gmail.com>
2020-09-04 11:50:11 -0500

    string reverse

---
+++ src/Java/StringReverse.java
@@ -0,0 +1,16 @@
+package Java;
+
+import java.io.*;
+import java.util.*;
+
+public class StringReverse {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+
+        System.out.println( A.equals( new StringBuilder(A).reverse().toString())
+                ? "Yes" : "No" );
+    }
+}
